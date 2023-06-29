66ed5be5228cd4d462f782c66110d21cbe456055
JJayJohnny <jasiek.barczewski@gmail.com>
2023-06-29 19:05:52 +0200

    quit function now removes all temp files

--- slash/quit.js
+++ slash/quit.js
@@ -1,5 +1,9 @@
 const {SlashCommandBuilder} = require("@discordjs/builders")
 const {useQueue} = require("discord-player")
+const fs = require('fs')
+const path = require('path')
+
+const directory = 'tempAudio'
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,8 +12,19 @@ module.exports = {
     run: async ({client, interaction}) => {
         const queue = useQueue(interaction.guild.id)
 
-        if(queue)
+        if(queue){
             queue.delete()
+            //delete all temp files
+            fs.readdir(directory, (err, files) => {
+                if (err) throw err;
+              
+                for (const file of files) {
+                  fs.unlink(path.join(directory, file), (err) => {
+                    if (err) throw err;
+                  });
+                }
+            });
+        }
 
         await interaction.editReply("Bye")
 
